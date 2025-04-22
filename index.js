7aba88328d5dea1d88ba884ca6c751eb86adcecc
asielLucianoCarmona <lindtaylor13@outlook.es>
2025-04-22 10:54:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+// Solo permitir peticiones desde este dominio
+//const allowedOrigins = ["https://midominio.com"];
+
+/*app.use( esto lo dejo comentado porque no tengo el dominio que usara el backend 
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("No permitido por CORS"));
+      }
+    },
+  })
+);*/
+
+// Endpoint para enviar notificación
+app.post("/send-notification", async (req, res) => {
+  const { token, title, body } = req.body;
+
+  if (!token || !title || !body) {
+    return res.status(400).json({ error: "Faltan parámetros requeridos" });
+  }
+
+  const message = {
+    notification: { title, body },
+    token,
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    res.json({ success: true, response });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
